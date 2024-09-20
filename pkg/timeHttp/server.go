f8695856dc126090cf926e2d885d078b48a9a12d
MarwanRadwan7 <marwan.swe@outlook.com>
2024-09-20 11:00:06 +0300

    feat: Add json datetime endpoint

--- pkg/timeHttp/server.go
+++ pkg/timeHttp/server.go
@@ -6,15 +6,27 @@ import (
 	"time"
 )
 
-func GetTimeHandler() http.HandlerFunc {
+// getTimeJsonHandler returns an HTTP handler function that responds with the current time in json format.
+func getTimeJsonHandler() http.HandlerFunc {
 	currentTime := time.Now().Format(time.RFC822)
 
 	return func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/datetime" {
-			http.NotFound(w, r)
+
+		if r.Method != http.MethodGet {
+			w.WriteHeader(http.StatusMethodNotAllowed)
 			return
 		}
 
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintf(w, `{"datetime": "%s"}`, currentTime)
+	}
+}
+
+// getTimePlainHandler returns an HTTP handler function that responds with the current time in plain text.
+func getTimePlainHandler() http.HandlerFunc {
+	currentTime := time.Now().Format(time.RFC822)
+
+	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodGet {
 			w.WriteHeader(http.StatusMethodNotAllowed)
 			return
@@ -25,6 +37,9 @@ func GetTimeHandler() http.HandlerFunc {
 }
 
 func StartServer(port string) error {
-	http.HandleFunc("/datetime", GetTimeHandler())
+
+	http.HandleFunc("/datetime/plain", getTimePlainHandler())
+	http.HandleFunc("/datetime/json", getTimeJsonHandler())
+
 	return http.ListenAndServe(":"+port, nil)
 }
--- pkg/timeHttp/server_test.go
+++ pkg/timeHttp/server_test.go
@@ -1,16 +1,20 @@
 package timehttp
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
-func TestGetTimeHandler(t *testing.T) {
-	handler := GetTimeHandler()
+func TestGetTimePlainHandler(t *testing.T) {
+	handler := getTimePlainHandler()
 
 	t.Run("Valid Request", func(t *testing.T) {
-		req := httptest.NewRequest(http.MethodGet, "/datetime", nil)
+		expected := time.Now().Format(time.RFC822)
+
+		req := httptest.NewRequest(http.MethodGet, "/datetime/plain", nil)
 		rr := httptest.NewRecorder()
 
 		handler.ServeHTTP(rr, req)
@@ -19,20 +23,57 @@ func TestGetTimeHandler(t *testing.T) {
 			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
 		}
 
+		got := rr.Body.String()
+
+		if got != expected {
+			t.Errorf("handler returned unexpected datetime: got %v want %v", got, expected)
+		}
 	})
-	t.Run("Invalid Path", func(t *testing.T) {
-		req := httptest.NewRequest(http.MethodGet, "/invalid", nil)
+
+	t.Run("Invalid Method", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodPost, "/datetime/plain", nil)
 		rr := httptest.NewRecorder()
 
 		handler.ServeHTTP(rr, req)
 
-		if status := rr.Code; status != http.StatusNotFound {
-			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusNotFound)
+		if status := rr.Code; status != http.StatusMethodNotAllowed {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusMethodNotAllowed)
+		}
+	})
+}
+
+func TestGetTimeJsonHandler(t *testing.T) {
+	handler := getTimeJsonHandler()
+
+	t.Run("Valid Request", func(t *testing.T) {
+		expected := time.Now().Format(time.RFC822)
+		req := httptest.NewRequest(http.MethodGet, "/datetime/json", nil)
+		rr := httptest.NewRecorder()
+
+		handler.ServeHTTP(rr, req)
+
+		var timeResponse struct {
+			Datetime string `json:"datetime"`
 		}
 
+		err := json.Unmarshal(rr.Body.Bytes(), &timeResponse)
+		if err != nil {
+			t.Fatalf("error unmarshalling response: %v", err)
+		}
+
+		got := timeResponse.Datetime
+
+		if status := rr.Code; status != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+		}
+
+		if got != expected {
+			t.Errorf("handler returned unexpected datetime: got %v want %v", got, expected)
+		}
 	})
+
 	t.Run("Invalid Method", func(t *testing.T) {
-		req := httptest.NewRequest(http.MethodPost, "/datetime", nil)
+		req := httptest.NewRequest(http.MethodPost, "/datetime/json", nil)
 		rr := httptest.NewRecorder()
 
 		handler.ServeHTTP(rr, req)
@@ -42,4 +83,5 @@ func TestGetTimeHandler(t *testing.T) {
 		}
 
 	})
+
 }
